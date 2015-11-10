8d4520f34ee8948d263b5fc8f821ab60d52c464a
xumin <xumin_wlt@163.com>
2015-11-10 17:58:44 +0800

    读书笔记《并发编程的艺术》

--- j360-jdk-thread/src/main/java/me/j360/jdk/concurrent/forkjoin/CountTask.java
+++ j360-jdk-thread/src/main/java/me/j360/jdk/concurrent/forkjoin/CountTask.java
@@ -1,5 +1,8 @@
 package me.j360.jdk.concurrent.forkjoin;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
 
 /**
@@ -21,6 +24,37 @@ public class CountTask extends RecursiveTask<Integer> {
     }
     @Override
     protected Integer compute() {
-        return null;
+        int sum = 0;
+        boolean canCompute = (end-start) <= THRESHOLD;
+        if(canCompute){
+            for(int i=start;i<=end;i++){
+                sum += i;
+            }
+        }else{
+            int middle = (start + end )/2;
+            CountTask leftTask = new CountTask(start,middle);
+            CountTask rightTask = new CountTask(middle,end);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+
+            sum = leftResult + rightResult;
+
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask task = new CountTask(1,4);
+        Future<Integer> result = forkJoinPool.submit(task);
+        try {
+            System.out.println(result.get());
+        }catch (Exception e){
+
+        }
     }
 }
